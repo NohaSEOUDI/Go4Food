73e4bad46f65637a6a3812aa15998d1969e533ad
Seoudi Noha <noha.seoudi@etu.umontpellier.fr>
2022-05-22 08:43:56 +0200

    update client interface

--- app/src/main/java/fr/nohas/go4food/Adaptor/HomeVerAdapter.java
+++ app/src/main/java/fr/nohas/go4food/Adaptor/HomeVerAdapter.java
@@ -1,26 +1,25 @@
 package fr.nohas.go4food.Adaptor;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 import java.util.ArrayList;
-import java.util.List;
 
 import fr.nohas.go4food.R;
-import fr.nohas.go4food.client.interfaces.UpdateVerticalRec;
-import fr.nohas.go4food.client.models.HomeHorModel;
 import fr.nohas.go4food.client.models.HomeVerModel;
 
 public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHolder> {
-   // UpdateVerticalRec updateVerticalRec;
+   private BottomSheetDialog bottomSheetDialog; //botton s'apparait en cliquant
     Context context;
     ArrayList<HomeVerModel> list;
     boolean check = true;
@@ -38,8 +37,50 @@ public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final int mImage = list.get(position).getImage();
+        final String mName = list.get(position).getName();
+        final String mTiming = list.get(position).getTiming();
+        final String mRating = list.get(position).getRating();
+        final String mPrice = list.get(position).getPrice();
+
+
         holder.imageView.setImageResource(list.get(position).getImage());
         holder.name.setText(list.get(position).getName());
+        holder.timing.setText(list.get(position).getTiming());
+        holder.rating.setText(list.get(position).getRating());
+        holder.price.setText(list.get(position).getPrice());
+
+        //pour rendre les itemes clickable
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetDialog = new BottomSheetDialog(context , R.style.BottomSheetTheme);
+                View sheetView = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_layout,null);
+
+                //pour le button
+                sheetView.findViewById(R.id.add_to_cart).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(context,"Added to Cart",Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                ImageView bottomImg = sheetView.findViewById(R.id.bottom_sheet_img);
+                TextView bottomName = sheetView.findViewById(R.id.bottom_sheet_name);
+                TextView bottomTiming = sheetView.findViewById(R.id.bottom_sheet_timing);
+                TextView bottomRating = sheetView.findViewById(R.id.bottom_sheet_rating);
+                TextView bottomPrice = sheetView.findViewById(R.id.bottom_sheet_price);
+
+                bottomImg.setImageResource(mImage);
+                bottomName.setText(mName);
+                bottomTiming.setText(mTiming);
+                bottomRating.setText(mRating);
+                bottomPrice.setText(mPrice);
+
+                bottomSheetDialog.setContentView(sheetView);
+                bottomSheetDialog.show();
+            }
+        });
     }
 
     @Override
